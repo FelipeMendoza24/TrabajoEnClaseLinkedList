3231a9f57cb348333781c2db81f5d3bc997d9b32
FelipeMendoza24 <felipemena@unisabana.edu.co>
2021-02-22 21:17:37 -0500

    srs

---
+++ src/main/java/collections/CourseLinkedList.java
@@ -0,0 +1,68 @@
+package collections;
+import entities.Student;
+import java.util.LinkedList;
+
+public class CourseLinkedList implements CourseCollectionsExample {
+    public static final int MAXIMUM_QUOTA = 30;
+
+    private LinkedList<Student> students;
+
+    public CourseLinkedList(){
+        students = new LinkedList<>();
+    }
+
+    @Override
+    public int numMaxStudents() {
+        return MAXIMUM_QUOTA;
+    }
+
+    @Override
+    public int numRegisteredStudents() {
+
+        return students.size();
+    }
+
+    @Override
+    public boolean addStudent(Student s) {
+        boolean result = false;
+        boolean exists = false;
+
+        if (numRegisteredStudents() < MAXIMUM_QUOTA) {
+            for (int i = 0; i < numRegisteredStudents() && !exists; i++) {
+                if (students.get(i).equals(s)) {
+                    exists = true;
+                }
+            }
+
+            if (!exists) {
+                students.add(s);
+                result = true;
+            }
+        }
+
+        return result;
+    }
+
+    @Override
+    public boolean removeStudent(Student s) {
+
+        return students.remove(s);
+    }
+
+    @Override
+    public Student getStudent(Student s) {
+        int i;
+        boolean found = false;
+        for (i = 0; i < numRegisteredStudents() && !found; i++) {
+            found = students.contains(s);
+        }
+        return found ? students.get(i - 1) : null;
+
+    }
+
+    @Override
+    public Student updateStudent(Student s) {
+        return null;
+    }
+
+}
